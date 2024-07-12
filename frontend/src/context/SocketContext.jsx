c5fa0aaa371f3287339a75a8ccad85c0e534d072
PoTaTo-boy-00 <ayanangshudutta1@gmail.com>
2024-07-13 00:04:27 +0530

    Socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://rt-chat-app-7kvl.onrender.com", {
         query: {
           userId: authUser.id,
         },
